7f9a103f73561e8216d4e61a6f2dc68f3d749ca3
권뉴나 <152950017+yunha-kwon@users.noreply.github.com>
2024-02-17 18:05:49 +0900

    practice

---
+++ 괄호.py
@@ -0,0 +1,22 @@
+#괄호 문제
+stack = []
+b = '({[]})()(()){{}}'
+
+for i in b:
+    if i == '(' or i == '{' or i == '[':
+        stack.append(i)
+    elif stack and i == ')' and stack[-1] == '(':
+        stack.pop()
+    elif stack and i == '}' and stack[-1] == '{':
+        stack.pop()
+    elif stack and i == ']' and stack[-1] == '[':
+        stack.pop()
+    elif i == ')' or i == '}' and i == ']':
+        stack.append(i)
+
+    if stack:
+        answer = 'false'
+    else:
+        answer = 'true'
+
+print(answer) #true
